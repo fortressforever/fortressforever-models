34875355214453adedb9bace11987bce7f9a45a2
WillWowmc <williamgeorgeson@hotmail.com>
2016-06-08 18:09:20 +1000

    Super Shotgun + Tranq anim edits

---
+++ weapons/supershotgun/viewmodel/compile.bat
@@ -0,0 +1,4 @@
+@echo off
+"%SourceSDK%/bin/ep1/bin/studiomdl" v_supershotgun.qc
+echo Compile attempted [super shotgun]! Check folder for new models and correct file size (.mdl should be around 37kb)
+pause
---
+++ weapons/tranq/viewmodel/compile.bat
@@ -0,0 +1,4 @@
+@echo off
+"%SourceSDK%/bin/ep1/bin/studiomdl" v_tranq.qc
+echo Compile attempted [tranq]! Check folder for new models and correct file size (should be around 49kb)
+pause
